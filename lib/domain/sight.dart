b7e2ae02a5503112e803bc0514311598d83ab106
musovvir <mr.muzagov@mail.ru>
2022-06-21 11:33:15 +0300

    add sights

---
+++ lib/domain/sight.dart
@@ -0,0 +1,17 @@
+class Sight {
+  String? name;
+  String? lat;
+  String? lon;
+  String? url;
+  String? details;
+  String? type;
+
+  Sight({
+    required this.name,
+    required this.lat,
+    required this.lon,
+    required this.url,
+    required this.details,
+    required this.type,
+  });
+}
